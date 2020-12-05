57f78a287553f89d1dd28b699ab1f55f56f4ec2c
julianbooher <julianbooher@hotmail.com>
2020-12-04 19:42:44 -0600

    got components hooked up, text on the image, need to hide it and set up the display feature

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
+// THIS IS WHAT DATA IS IN THE galleryItem
+// {"id":1,"path":"images/Donkey.jpg","description":"Photo of a goat taken at Glacier National Park.","likes":0}
+
+
 class GalleryItem extends Component{
 
     state = {
         // State for if this has been clicked on, and if it's true, it will display a description of the photo.
-        selected: false
+        selected: false,
+        imgPath: `../../${this.props.galleryItem.path}`
     }
     render(){
         return(
             <div className="gallery-item">
-                {JSON.stringify(this.props.galleryItem)}
+                <div className="gallery-image-text">TEXT</div>
+                
+                <img className="gallery-image" src={"../../" + this.props.galleryItem.path} alt={this.props.galleryItem.description}/>
             </div>
         ) // end return
     } // end render
